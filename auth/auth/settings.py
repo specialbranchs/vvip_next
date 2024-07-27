69592da0c4772b91cc14ccb5ac4e59d7813c0553
Faridul <fislam@Fariduls-MacBook-Pro.local>
2024-07-27 12:08:00 +0600

    D

--- auth/auth/settings.py
+++ auth/auth/settings.py
@@ -85,18 +85,18 @@ WSGI_APPLICATION = 'auth.wsgi.application'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
     # 'default': {
-    #     'ENGINE': 'django.db.backends.mysql',
-    #     'NAME': 'vvip_root',
-    #     'USER':'root',
-    #     'PASSWORD':'sb@db#$2024',
-    #     'HOST':'127.0.0.1',
-    #     'PORT':'3306'
+    #     'ENGINE': 'django.db.backends.sqlite3',
+    #     'NAME': BASE_DIR / 'db.sqlite3',
     # }
+    'default': {
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'vvip_root',
+        'USER':'root',
+        'PASSWORD':'sb@db#$2024',
+        'HOST':'127.0.0.1',
+        'PORT':'3306'
+    }
   
 }
 
